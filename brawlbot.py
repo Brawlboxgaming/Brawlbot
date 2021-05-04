71f1496f3cdde6235d536646351c4b1284acd2af
Brawlboxgaming <zombielord343@gmail.com>
2021-05-04 18:07:46 +0100

    Optimised functionality

--- brawlbot.py
+++ brawlbot.py
@@ -1,47 +1,40 @@
-'''Brawlbot made by Brawlbox for The Brawl Box on discord
-If you would like to use this bot, please make sure to credit Brawlbox'''
+'''Brawlbot made by Brawlbox
+If you would like to use this code, please make sure to credit Brawlbox'''
 
 import discord
-# import logging
 from discord.ext import commands
 
 f = open("token.txt", "r")
 token = f.readline()
 f.close()
 
-# logging.basicConfig(level=logging.INFO)
-
 bot = commands.Bot(command_prefix='>')
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity=discord.Activity(name="helping @Brawlbox", type=discord.ActivityType.playing))
+    await bot.change_presence(activity=discord.Activity(name="helping 🥊Brawlbox🥊", type=discord.ActivityType.playing))
 
 async def process_category(category):
+    n = 1
     numempty = 0
     templatename = None
     for c in category.channels:
-        if len(c.members) == 0:
-            numempty += 1
-        if c.name.endswith(" 1"):
-            templatename = c.name[:-2]
-        if c.name.endswith(" 1🔒"):
-            templatename = c.name[:-3]
+        if isinstance(c, discord.VoiceChannel):
+            if len(c.members) == 0:
+                numempty += 1
+            endchars = (c.name[c.name.index(str(n)):])[1:]
+            templatename = c.name[:-(len(endchars)+1)]
+            n += 1
 
     if numempty == 0:
         # Create a channel
         already = True
-        n = 1
         while already:
-            name = templatename + " " + str(n)
-            if category.name == "DUO PRIVATE CHANNELS":
-                user_limit = 2
-                name += "🔒"
-            elif category.name == "TRIO PRIVATE CHANNELS":
-                user_limit = 3
-                name += "🔒"
-            else:
-                user_limit = None
+            name = templatename + str(n) + endchars
+            for c in category.channels:
+                if isinstance(c, discord.VoiceChannel):
+                    user_limit = c.user_limit
+                    break
             already = False
             for c in category.channels:
                 if c.name == name:
